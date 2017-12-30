4ae40dfebe1697743e97fc2b249e8824bf5278df
snookyfag <cybermsd1313@gmail.com>
2017-12-30 14:32:10 +0700

    modify files

--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -4,11 +4,20 @@ Template.postSubmit.events({
 		e.preventDefault();
 
 		var post = {
-			url : $(e.target).find(['name=url']).value(),
-			title: $(e.target).find(['name=title']).value()
+			url : $(e.target).find(['name=url']).val(),
+			title: $(e.target).find(['name=title']).val()
 		};
 
-		post._id = Posts.insert(post);
-		Router.go('postPage', post);
+		Meteor.call('postInsert', post, function(error, result){
+			// display the error to the user and abort
+			if(error)
+				return alert(error.reason);
+
+			// show this result but route anyway
+			if(result.postExists)
+				alert('This link have already been posted');
+
+			Router.go('postPage', {_id: result._id});
+		});
 	}
 });
