90c0de35051c8be839314403ed60ea19904f24fc
snookyfag <cybermsd1313@gmail.com>
2017-12-30 11:23:16 +0700

    remove insecure package, Allowed Post collection insert with userId

--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -4,9 +4,9 @@ Template.postSubmit.events({
 		e.preventDefault();
 
 		var post = {
-			url : $(e.target).find(['name=url']).value();
-			title: $(e.target).find(['name=title']).value();
-		}
+			url : $(e.target).find(['name=url']).value(),
+			title: $(e.target).find(['name=title']).value()
+		};
 
 		post._id = Posts.insert(post);
 		Router.go('postPage', post);
