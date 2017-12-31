1afdbf51262772fdc1a6d5ebf3fdefc21cb252dc
snookyfag <cybermsd1313@gmail.com>
2017-12-31 10:39:32 +0700

    modified

--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -4,8 +4,8 @@ Template.postSubmit.events({
 		e.preventDefault();
 
 		var post = {
-			url : $(e.target).find(['name=url']).val(),
-			title: $(e.target).find(['name=title']).val()
+			url : $(e.target).find('[name=url]').val(),
+			title: $(e.target).find('[name=title]').val()
 		};
 
 		Meteor.call('postInsert', post, function(error, result){
